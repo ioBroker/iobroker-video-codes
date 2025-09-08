8c508ceb321a370a51dbc255266c269f8ae8f654
Bluefox <dogafox@gmail.com>
2025-09-08 12:00:05 +0200

    Parse input JSON and clean up text format

--- 2025-09-08_ioBroker_plus_n8n/3_parser.js
+++ 2025-09-08_ioBroker_plus_n8n/3_parser.js
@@ -1,1 +1,7 @@
-
+let text = $input.first().json.output;
+if (typeof text === 'string') {
+  text = text.replaceAll('```', '');
+  text = text.replace(/^json/, '');
+  text = JSON.parse(text);
+}
+return text.map(it => ({ json: it }));
